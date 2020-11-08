74cc11d626b49d2d8954360e872bb660845ebc2d
nikukeke <waiwaiguoke@sina.com>
2020-11-08 22:14:11 +0800

    week03

---
+++ week03/11全排列46.js
@@ -0,0 +1,20 @@
+var permute = function (nums) {
+    const res = [];
+    const used = {};
+    dfs([])
+    function dfs(path) {
+        if (path.length == nums.length) {
+            res.push(path.slice())
+            return
+        }
+        for (const num of nums) {
+            if(used[num]) continue;
+            path.push(num);
+            used[num] = true;
+            dfs(path);
+            path.pop();
+            used[num] = false;
+        }
+    }
+    return res;
+};
