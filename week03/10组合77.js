79334e9239a12df861276f1df79eaa4c2ecb1990
waiwaiguoke <waiwaiguoke@sina.com>
2020-11-16 18:02:54 +0800

    week

--- week03/10组合77.js
+++ week03/10组合77.js
@@ -1,13 +1,23 @@
+// 解题思路
+// 1、递归 
+
 var combine = function (n, k) {
     const res = [];
+    // start是枚举选择的起点 path是当前构建的路径（组合）
     const helper = (start, path) => {
         if (path.length == k) {
+            // 拷贝一份path，推入res
             res.push(path.slice());
+            // 结束当前递归
             return;
         }
+        // 枚举出所有选择
         for (let i = start; i <= n; i++) {
+            // 选择
             path.push(i);
+            // 向下继续选择
             helper(i + 1, path);
+            // 撤销选择
             path.pop();
         }
     }
--- week03/8二叉树的最近公共祖先236.js
+++ week03/8二叉树的最近公共祖先236.js
@@ -1,30 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <script>
-
-        var lowestCommonAncestor = function (root, p, q) {
-            let ans;
-            const dfs = (root, p, q) => {
-                if (root === null) return false;
-                const lson = dfs(root.left, p, q);
-                const rson = dfs(root.right, p, q);
-                if ((lson && rson) || ((root.val == p.val || root.val == q.val) && (lson || rson))) {
-                    ans = root;
-                }
-                return lson || rson || (root.val == p.val || root.val == q.val);
-            }
-            dfs(root, p, q);
-            return ans;
-        };
-    </script>
-</body>
-
-</html>
+// 解题思路
+// 递归
+// 时间复杂度: O(n)
+// 空间复杂度: O(n)
+var lowestCommonAncestor = function (root, p, q) {
+    let ans;
+    const dfs = (root, p, q) => {
+        if (root === null) return false;
+        const lson = dfs(root.left, p, q);
+        const rson = dfs(root.right, p, q);
+        if ((lson && rson) || ((root.val == p.val || root.val == q.val) && (lson || rson))) {
+            ans = root;
+        }
+        return lson || rson || (root.val == p.val || root.val == q.val);
+    }
+    dfs(root, p, q);
+    return ans;
+};
