b7c4f497891396ce77540c7b33830c27fe7dd67a
nikukeke <waiwaiguoke@sina.com>
2020-12-26 00:00:10 +0800

    week03

--- week03/5二叉树的最大深度104.js
+++ week03/5二叉树的最大深度104.js
@@ -13,3 +13,20 @@ var maxDepth = function (root) {
 // 二、广度优先搜索
 // 时间复杂度：O(n)，其中n为二叉树的节点个数
 // 空间复杂度：此方法空间的消耗取决于队列存储的元素数量，其在最坏情况下会达到O(n)
+
+
+const maxDepth = (root) => {
+    if (root == null) return 0;
+    const queue = [root];
+    let depth = 1;
+    while (queue.length) {
+        const levelSize = queue.length;
+        for (let i = 0; i < levelSize; i++) {
+            const cur = queue.shift();
+            if (cur.left) queue.push(cur.left);
+            if (cur.right) queue.push(cur.right);
+        }
+        if (queue.length) depth++;
+    }
+    return depth;
+}
--- week03/6二叉树的最小深度111.js
+++ week03/6二叉树的最小深度111.js
@@ -21,3 +21,21 @@ var minDepth = function (root) {
 // 2、广度优先搜索
 // 时间复杂度：O(n)
 // 空间复杂度：O(n)
+
+var minDepth = (root) => {
+    if (root == null) return 0;
+    const queue = [root];
+    let depth = 1;
+    while (queue.length) {
+        const levelSize = queue.length;
+        for (let i = 0; i < levelSize; i++) {
+            const cur = queue.shift();
+            if (cur.left == null && cur.right == null) {
+                return depth;
+            }
+            if (cur.left) queue.push(cur.left);
+            if (cur.right) queue.push(cur.right);
+        }
+        depth++;
+    }
+}
